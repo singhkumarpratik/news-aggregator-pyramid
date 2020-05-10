cffb5909f9639f72ca3f4d6bedbc2303ec175ae9
Pratik <prxtiksingh@gmail.com>
2020-05-10 13:09:11 +0530

    completed Times of India scraper

--- news-aggregator/news_aggregator/__init__.py
+++ news-aggregator/news_aggregator/__init__.py
@@ -5,9 +5,9 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
-    config.include('pyramid_jinja2')
-    config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
-    config.add_route('paper', '/paper/{paperName}')
+    config.include("pyramid_jinja2")
+    config.add_static_view("static", "static", cache_max_age=3600)
+    config.add_route("home", "/")
+    config.add_route("paper", "/paper/{paperName}")
     config.scan()
     return config.make_wsgi_app()
--- news-aggregator/news_aggregator/views.py
+++ news-aggregator/news_aggregator/views.py
@@ -1,17 +1,59 @@
 from pyramid.view import view_config
-from .scrapers.toi import *
-papers={'toi':'Times of India','ht':'Hindustan Times'}
-@view_config(route_name='home', renderer='templates/index.jinja2')
-@view_config(route_name='paper', renderer='templates/index.jinja2')
-def my_view(request):
-    toi_news={}
-    try:
-        paperName=papers[request.matchdict['paperName']]
-        if request.matchdict['paperName']=='toi':
-            toi_news=toiIndia()
-            return {'paperName':paperName,'toi_news':toi_news}
-        if request.matchdict['paperName']=='ht':
-            return {'paperName':paperName,'toi_news':toi_news}
-    except:
-        paperName="Front Page"
-        return {'paperName':paperName,'toi_news':toi_news}
+import requests
+from bs4 import BeautifulSoup
+
+papers = {"toi": "Times of India", "ht": "Hindustan Times"}
+toiCategories = {
+    "world": "World News",
+    "india": "India News",
+    "sports": "Sports News",
+    "entertainment": "Entertainment News",
+}
+
+
+@view_config(route_name="home", renderer="templates/home.jinja2")
+def home(request):
+    paperName = "Front Page"
+    return {"paperName": paperName, "papers": papers}
+
+
+@view_config(route_name="paper", renderer="templates/index.jinja2")
+@view_config(
+    route_name="paper", renderer="templates/index.jinja2", request_method="POST"
+)
+def toi(request):
+    paperName = papers[request.matchdict["paperName"]]
+    if request.matchdict["paperName"] == "toi":
+        news = {}
+        if request.POST:
+            print(request.POST.get("category"))
+            toiRequestWorld = requests.get(
+                "https://timesofindia.indiatimes.com/briefs/"
+                + request.POST.get("category")
+            )
+
+        else:
+            toiRequestWorld = requests.get(
+                "https://timesofindia.indiatimes.com/briefs/world"
+            )
+        soup = BeautifulSoup(toiRequestWorld.content, "html.parser")
+        # headings = soup.find("div", {"class": "briefs_outer clearfix"})
+        allDetails = soup.find_all("div", {"class": "brief_box"})
+        for i in allDetails:
+            try:
+                title = i.h2.text
+                summary = i.p.text
+                url = i.a["href"]
+                imgSrc = i.find_all("div", {"class": "posrel"})[0].img["data-src"]
+                news[title] = [imgSrc, summary, url]
+            except:
+                continue
+        return {
+            "paperName": paperName,
+            "papers": papers,
+            "news": news,
+            "toiCategories": toiCategories,
+        }
+    if request.matchdict["paperName"] == "ht":
+        news = {}
+        return {"paperName": paperName, "papers": papers, "news": news}
